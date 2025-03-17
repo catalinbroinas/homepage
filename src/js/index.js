39dd310330a88d728605ce889b92152d8f37195c
Cătălin Broinaș <43112698+catalinbroinas@users.noreply.github.com>
2025-03-17 14:00:40 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import '../css/styles.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Template',
+            inject: 'body',
+            template: './src/template.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[hash][ext]',
+                },
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][hash][ext]',
+                },
+            },
+        ],
+    },
+    output: {
+        filename: 'js/main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
